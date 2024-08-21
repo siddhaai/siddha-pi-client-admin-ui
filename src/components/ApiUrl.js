424951f9430a6277f47c82b66d2af309a3aa4dba
Mohankumar Saraswathy <msaraswathy@siddhaai.com>
2024-08-21 17:04:52 +0530

    Update ApiUrl.js

--- src/components/ApiUrl.js
+++ src/components/ApiUrl.js
@@ -1,9 +1,9 @@
-// const prod = false;
+const prod = false;
 
-// export let ApiUrl = prod
-//   ? "https://siddha-pi-001-api.azurewebsites.net/"
-//   : "https://siddhaai-demo-api-1.azurewebsites.net";
+export let ApiUrl = prod
+   ? "https://siddha-pi-001-api.azurewebsites.net/"
+   : "http://ec2-3-221-146-175.compute-1.amazonaws.com:3000/";
 
-export let ApiUrl = `http://localhost:3000`;
+// export let ApiUrl = `http://localhost:3000`;
 // export let ApiUrl = `http://192.168.1.7:3000`; // room airtel
 // export let ApiUrl = `http://192.168.1.26:4000`; // office airtel
